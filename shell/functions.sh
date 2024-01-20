89f86f9783dda32b9baacde6f7b0ed86b6a6c5b4
Andres Felipe Ramirez Ocaña <aramirez@indigo.tech>
2024-01-20 13:13:29 -0500

    fix logging init

--- shell/functions.sh
+++ shell/functions.sh
@@ -4,16 +4,39 @@ function cdd() {
 
 function j() {
 	fname=$(declare -f -F _z)
-
 	[ -n "$fname" ] || source "$DOTLY_PATH/modules/z/z.sh"
-
 	_z "$1"
 }
 
 function recent_dirs() {
 	# This script depends on pushd. It works better with autopush enabled in ZSH
-	escaped_home=$(echo $HOME | sed 's/\//\\\//g')
+	escaped_home=$(echo "$HOME" | sed 's/\//\\\//g')
 	selected=$(dirs -p | sort -u | fzf)
 
 	cd "$(echo "$selected" | sed "s/\~/$escaped_home/")" || echo "Invalid directory"
+}
+
+function jwtd() {
+    #this script decode a jwt token
+    #it depends on jq
+    #usage: jwtd <token>
+    echo "$1" | cut -d "." -f 2 | base64 -d | jq .
+    # jq -R 'split(".") | .[1] | @base64d | fromjson' <<< "$1"
+
+}
+
+function jwte() {
+    #this script encode a jwt token
+    #it depends on jq
+    #usage: jwte <token>
+    echo "$1" | jq -R 'split(".") | .[1] | @base64d | fromjson' | jq -c .
+    # jq -R 'split(".") | .[1] | @base64d | fromjson' <<< "$1"
+
+}
+
+function pwdgen() {
+    #this script generate a random password with the given length
+    #it depends on pwgen
+    #usage: pwdgen <length>
+    pwgen -s -c -y -1 "$1"
 }
--- shell/init.sh
+++ shell/init.sh
@@ -1,5 +1,7 @@
 # This is a useful file to have the same aliases/functions in bash and zsh
 
+# Load all the shell files
+source "$DOTFILES_PATH/shell/logging.sh"
 source "$DOTFILES_PATH/shell/aliases.sh"
 source "$DOTFILES_PATH/shell/exports.sh"
 source "$DOTFILES_PATH/shell/private-exports.sh"
